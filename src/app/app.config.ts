4f8bcb2a2ecea5fdd2a9361a52e1f2dfe201c13f
Yaksh <kataria.yaksh2@gmail.com>
2026-03-24 22:44:07 +1100

    App styling and config

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,8 +1,9 @@
-import { ApplicationConfig, provideBrowserGlobalErrorListeners } from '@angular/core';
+import { ApplicationConfig, provideZonelessChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
+import { provideHttpClient } from '@angular/common/http';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideBrowserGlobalErrorListeners(), provideRouter(routes)],
+  providers: [provideZonelessChangeDetection(), provideRouter(routes), provideHttpClient()],
 };
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,16 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    redirectTo: 'todos',
+    pathMatch: 'full',
+  },
+  {
+    path: 'todos',
+    loadComponent: () =>
+      import('./features/todos/pages/todo-page/todo-page.component').then(
+        (m) => m.TodoPageComponent,
+      ),
+  },
+];
